921da9a1f958b1122498dc9501f7ac1da39c3e60
LatvianModder <latvianmodder@gmail.com>
2016-07-02 01:06:53 +0300

    Replaced many lang keys with vanilla keys

--- src/main/java/com/feed_the_beast/ftbl/api/client/gui/GuiLang.java
+++ src/main/java/com/feed_the_beast/ftbl/api/client/gui/GuiLang.java
@@ -4,42 +4,41 @@ import com.feed_the_beast.ftbl.api.LangKey;
 
 public class GuiLang
 {
-    public static final LangKey button_settings = get("button.settings");
-    public static final LangKey button_back = get("button.back");
-    public static final LangKey button_up = get("button.up");
-    public static final LangKey button_down = get("button.down");
-    public static final LangKey button_prev = get("button.prev");
-    public static final LangKey button_next = get("button.next");
-    public static final LangKey button_cancel = get("button.cancel");
-    public static final LangKey button_accept = get("button.accept");
-    public static final LangKey button_add = get("button.add");
-    public static final LangKey button_remove = get("button.remove");
-    public static final LangKey button_close = get("button.close");
-    public static final LangKey button_save = get("button.save");
-    public static final LangKey button_refresh = get("button.refresh");
-    public static final LangKey button_edit = get("button.edit");
-    public static final LangKey button_info = get("button.info");
-    public static final LangKey label_server_forced = get("label.server_forced");
-    public static final LangKey label_true = get("label.true");
-    public static final LangKey label_false = get("label.false");
-    public static final LangKey label_enabled = get("label.enabled");
-    public static final LangKey label_disabled = get("label.disabled");
-    public static final LangKey label_on = get("label.on");
-    public static final LangKey label_off = get("label.off");
-    public static final LangKey label_online = get("label.online");
-    public static final LangKey label_offline = get("label.offline");
-    public static final LangKey label_whitelist = get("label.whitelist");
-    public static final LangKey label_blacklist = get("label.blacklist");
-    public static final LangKey label_friend = get("label.friend");
-    public static final LangKey label_friend_pending = get("label.friend.pending");
-    public static final LangKey label_friend_new = get("label.friend.new");
-    public static final LangKey label_friend_new_click = get("label.friend.new_click");
-    public static final LangKey label_friend_joined = get("label.friend.joined");
-    public static final LangKey label_friend_last_seen = get("label.friend.last_seen");
-    public static final LangKey label_friend_deaths = get("label.friend.deaths");
+    public static final LangKey button_done = new LangKey("gui.done");
+    public static final LangKey button_cancel = new LangKey("gui.cancel");
+    public static final LangKey button_back = new LangKey("gui.back");
+    public static final LangKey button_up = new LangKey("gui.up");
+    public static final LangKey button_down = new LangKey("gui.down");
+    public static final LangKey button_yes = new LangKey("gui.yes");
+    public static final LangKey button_no = new LangKey("gui.no");
+    public static final LangKey button_none = new LangKey("gui.none");
+    public static final LangKey button_all = new LangKey("gui.all");
+    public static final LangKey button_edit = new LangKey("selectServer.edit");
+    public static final LangKey button_options = new LangKey("options.title");
+    public static final LangKey button_prev_page = new LangKey("createWorld.customize.custom.prev");
+    public static final LangKey button_next_page = new LangKey("createWorld.customize.custom.next");
+    public static final LangKey button_defaults = new LangKey("createWorld.customize.custom.defaults");
+    public static final LangKey button_delete = new LangKey("selectServer.delete");
+    public static final LangKey button_refresh = new LangKey("selectServer.refresh");
 
-    private static LangKey get(String s)
-    {
-        return new LangKey("gui_lm." + s);
-    }
+    public static final LangKey label_enabled = new LangKey("addServer.resourcePack.enabled");
+    public static final LangKey label_disabled = new LangKey("addServer.resourcePack.disabled");
+    public static final LangKey label_on = new LangKey("options.on");
+    public static final LangKey label_off = new LangKey("options.off");
+
+    public static final LangKey button_accept = new LangKey("gui_lm.button.accept");
+    public static final LangKey button_add = new LangKey("gui_lm.button.add");
+    public static final LangKey button_close = new LangKey("gui_lm.button.close");
+    public static final LangKey button_save = new LangKey("gui_lm.button.save");
+    public static final LangKey button_info = new LangKey("gui_lm.button.info");
+
+    public static final LangKey label_other = new LangKey("gui_lm.label.other");
+    public static final LangKey label_server_forced = new LangKey("gui_lm.label.server_forced");
+    public static final LangKey label_online = new LangKey("gui_lm.label.online");
+    public static final LangKey label_offline = new LangKey("gui_lm.label.offline");
+    public static final LangKey label_whitelist = new LangKey("gui_lm.label.whitelist");
+    public static final LangKey label_blacklist = new LangKey("gui_lm.label.blacklist");
+    public static final LangKey label_friend_joined = new LangKey("gui_lm.label.friend.joined");
+    public static final LangKey label_friend_last_seen = new LangKey("gui_lm.label.friend.last_seen");
+    public static final LangKey label_friend_deaths = new LangKey("gui_lm.label.friend.deaths");
 }
