073d72b6e8470ce636e8632bb84fa5ec0cc59255
LatvianModder <latvianmodder@gmail.com>
2017-08-26 14:54:29 +0300

    Hopefully fixed weird teleporting issues by using vanilla/forge method

--- src/main/lib/com/feed_the_beast/ftbl/lib/util/ServerUtils.java
+++ src/main/lib/com/feed_the_beast/ftbl/lib/util/ServerUtils.java
@@ -11,7 +11,6 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.AxisAlignedBB;
@@ -83,82 +82,23 @@ public class ServerUtils
 		}
 	}
 
-	public static void teleportPlayer(Entity entity, BlockDimPos pos)
+	public static void teleportPlayer(EntityPlayerMP player, BlockDimPos pos)
 	{
-		teleportPlayer(entity, pos.toVec(), pos.dim);
+		teleportPlayer(player, pos.toVec(), pos.dim);
 	}
 
-	public static void teleportPlayer(Entity entity, Vec3d pos, int dim)
+	public static void teleportPlayer(EntityPlayerMP player, Vec3d pos, int dim)
 	{
-		entity.fallDistance = 0F;
-		EntityPlayerMP player = entity instanceof EntityPlayer ? (EntityPlayerMP) entity : null;
-
-		if (dim == entity.dimension)
+		if (dim != player.dimension)
 		{
-			if (pos.x == entity.posX && pos.y == entity.posY && pos.z == entity.posZ)
-			{
-				return;
-			}
-
-			if (player != null)
-			{
-				player.connection.setPlayerLocation(pos.x, pos.y, pos.z, player.rotationYaw, player.rotationPitch);
-				return;
-			}
+			WorldServer toDim = player.mcServer.getWorld(dim);
+			player.mcServer.getPlayerList().transferPlayerToDimension(player, toDim.provider.getDimension(), new TeleporterBlank(toDim));
 		}
 
-		//FTBLib.dev_logger.info("Teleporting " + entity + " from " + new EntityPos(entity) + " to " + new EntityPos(x, y, z, dim));
-		int from = entity.dimension;
-		float rotationYaw = entity.rotationYaw;
-		float rotationPitch = entity.rotationPitch;
-		MinecraftServer server = ServerUtils.getServer();
-		WorldServer fromDim = server.getWorld(from);
-		WorldServer toDim = server.getWorld(dim);
-
-		if (player != null)
-		{
-			server.getPlayerList().transferPlayerToDimension(player, dim, new TeleporterBlank(toDim));
-			if (from == 1 && entity.isEntityAlive())
-			{
-				// getMode around vanilla End hacks
-				toDim.spawnEntity(entity);
-				toDim.updateEntityWithOptionalForce(entity, false);
-			}
-		}
-		else
-		{
-			NBTTagCompound tagCompound = new NBTTagCompound();
-			entity.writeToNBT(tagCompound);
-			Class<? extends Entity> entityClass = entity.getClass();
-			fromDim.removeEntity(entity);
-
-			try
-			{
-				Entity newEntity = entityClass.getConstructor(World.class).newInstance(toDim);
-				newEntity.readFromNBT(tagCompound);
-				newEntity.setLocationAndAngles(pos.x, pos.y, pos.z, rotationYaw, rotationPitch);
-				newEntity.forceSpawn = true;
-				toDim.spawnEntity(newEntity);
-				newEntity.forceSpawn = false;
-			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
-			}
-		}
-
-		entity.fallDistance = 0;
-		entity.rotationYaw = rotationYaw;
-		entity.rotationPitch = rotationPitch;
-
-		if (player != null)
-		{
-			player.setPositionAndUpdate(pos.x, pos.y, pos.z);
-		}
-		else
-		{
-			entity.setPosition(pos.x, pos.y, pos.z);
-		}
+		player.connection.setPlayerLocation(pos.x, pos.y, pos.z, player.rotationYaw, player.rotationPitch);
+		player.motionX = player.motionY = player.motionZ = 0D;
+		player.fallDistance = 0F;
+		player.world.updateEntityWithOptionalForce(player, true);
 	}
 
 	public static double getMovementFactor(int dim)
@@ -267,7 +207,7 @@ public class ServerUtils
 	{
 		if (player == null)
 		{
-			for (EntityPlayer player1 : ServerUtils.getServer().getPlayerList().getPlayers())
+			for (EntityPlayer player1 : getServer().getPlayerList().getPlayers())
 			{
 				player1.sendStatusMessage(component, true);
 			}
