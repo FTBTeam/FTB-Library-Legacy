255b7afa473b7e4554649c0dbe18e5460d98da7d
MinecraftXwinP <tony84727@gmail.com>
2018-05-06 16:18:05 +0800

    Added usernameFilter to GuiManagePlayersBase.

--- src/main/java/com/feed_the_beast/ftblib/client/teamsgui/GuiManagePlayersBase.java
+++ src/main/java/com/feed_the_beast/ftblib/client/teamsgui/GuiManagePlayersBase.java
@@ -1,7 +1,6 @@
 package com.feed_the_beast.ftblib.client.teamsgui;
 
-import com.feed_the_beast.ftblib.lib.gui.Panel;
-import com.feed_the_beast.ftblib.lib.gui.SimpleTextButton;
+import com.feed_the_beast.ftblib.lib.gui.*;
 import com.feed_the_beast.ftblib.lib.gui.misc.GuiButtonListBase;
 import com.feed_the_beast.ftblib.lib.icon.Color4I;
 import com.feed_the_beast.ftblib.lib.icon.Icon;
@@ -60,6 +59,7 @@ public class GuiManagePlayersBase extends GuiButtonListBase
 
 	private final List<MessageMyTeamPlayerList.Entry> entries;
 	private final BiFunction<Panel, MessageMyTeamPlayerList.Entry, ButtonPlayerBase> buttonFunction;
+	private String usernameFilter = "";
 
 	public GuiManagePlayersBase(String title, Collection<MessageMyTeamPlayerList.Entry> m, BiFunction<Panel, MessageMyTeamPlayerList.Entry, ButtonPlayerBase> b)
 	{
@@ -68,12 +68,23 @@ public class GuiManagePlayersBase extends GuiButtonListBase
 		buttonFunction = b;
 	}
 
+	public void setFilter (String username) {
+		this.usernameFilter = username;
+	}
+
+	private List<MessageMyTeamPlayerList.Entry> applyFilter (Collection<MessageMyTeamPlayerList.Entry> list) {
+		final ArrayList<MessageMyTeamPlayerList.Entry> toFilter = new ArrayList(list);
+		toFilter.removeIf(e -> !e.name.startsWith(this.usernameFilter));
+		return toFilter;
+	}
+
 	@Override
 	public void addButtons(Panel panel)
 	{
-		entries.sort(null);
+		final List<MessageMyTeamPlayerList.Entry> players = applyFilter(entries);
+		players.sort(null);
 
-		for (MessageMyTeamPlayerList.Entry m : entries)
+		for (MessageMyTeamPlayerList.Entry m : players)
 		{
 			panel.add(buttonFunction.apply(panel, m));
 		}
