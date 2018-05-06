832ba2939c4b80be21f59b12e12b1e93f5261c95
LatvianModder <latvianmodder@gmail.com>
2018-05-07 02:35:06 +0300

    Fixed loading icon resetting animation

--- src/main/java/com/feed_the_beast/ftblib/lib/icon/LoadingIcon.java
+++ src/main/java/com/feed_the_beast/ftblib/lib/icon/LoadingIcon.java
@@ -3,6 +3,7 @@ package com.feed_the_beast.ftblib.lib.icon;
 import com.feed_the_beast.ftblib.FTBLib;
 import com.feed_the_beast.ftblib.lib.client.ClientUtils;
 import com.feed_the_beast.ftblib.lib.gui.GuiHelper;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.ResourceLocation;
 
@@ -21,17 +22,16 @@ public class LoadingIcon extends Icon
 	@Override
 	public void draw(int x, int y, int w, int h, Color4I col)
 	{
-		long t = System.currentTimeMillis() % 3600L;
 		col = col.whiteIfEmpty();
 		ClientUtils.MC.getTextureManager().bindTexture(TEXTURE);
 		GlStateManager.pushMatrix();
 		GlStateManager.translate(x + w / 2D, y + h / 2D, 0D);
 		GlStateManager.pushMatrix();
-		GlStateManager.rotate(t * 0.3F, 0F, 0F, 1F);
+		GlStateManager.rotate((float) (Minecraft.getSystemTime() * 0.3D), 0F, 0F, 1F);
 		GuiHelper.drawTexturedRect(-w / 2, -h / 2, w, h, col, 0D, 0D, 1D, 1D);
 		GlStateManager.popMatrix();
 		GlStateManager.pushMatrix();
-		GlStateManager.rotate(t * -0.13F, 0F, 0F, 1F);
+		GlStateManager.rotate((float) (Minecraft.getSystemTime() * -0.13D), 0F, 0F, 1F);
 		GlStateManager.scale(0.5D, 0.5D, 1D);
 		GuiHelper.drawTexturedRect(-w / 2, -h / 2, w, h, col, 0D, 0D, 1D, 1D);
 		GlStateManager.popMatrix();
