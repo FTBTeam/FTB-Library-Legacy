abb430e6900a672e9248deb904a2843536e1bf41
LatvianModder <latvianmodder@gmail.com>
2018-05-10 02:46:23 +0300

    Changed OpenGuideEvent into GuideEvent.Open and GuideEvent.Check

---
+++ src/main/java/com/feed_the_beast/ftblib/events/client/GuideEvent.java
@@ -0,0 +1,49 @@
+package com.feed_the_beast.ftblib.events.client;
+
+import com.feed_the_beast.ftblib.events.FTBLibEvent;
+import net.minecraftforge.fml.common.eventhandler.Cancelable;
+
+/**
+ * @author LatvianModder
+ */
+@Cancelable
+public class GuideEvent extends FTBLibEvent
+{
+	public static boolean check(String path)
+	{
+		return new Check(path).post();
+	}
+
+	public static boolean open(String path)
+	{
+		return new Open(path).post();
+	}
+
+	private final String path;
+
+	private GuideEvent(String p)
+	{
+		path = p;
+	}
+
+	public static class Check extends GuideEvent
+	{
+		private Check(String path)
+		{
+			super(path);
+		}
+	}
+
+	public static class Open extends GuideEvent
+	{
+		private Open(String path)
+		{
+			super(path);
+		}
+	}
+
+	public String getPath()
+	{
+		return path;
+	}
+}
