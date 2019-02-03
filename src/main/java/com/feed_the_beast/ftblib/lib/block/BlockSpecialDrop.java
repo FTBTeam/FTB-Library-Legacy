efd4e8d2ea3db4d7c8e6685e4162e4a1d71bcf9e
LatvianModder <latvianmodder@gmail.com>
2019-02-03 12:01:02 +0200

    Fixed special drop blocks

--- src/main/java/com/feed_the_beast/ftblib/lib/block/BlockSpecialDrop.java
+++ src/main/java/com/feed_the_beast/ftblib/lib/block/BlockSpecialDrop.java
@@ -5,19 +5,11 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Enchantments;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.stats.StatList;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-
-import javax.annotation.Nullable;
 
 /**
  * @author LatvianModder
@@ -27,35 +19,6 @@ public class BlockSpecialDrop extends Block
 	public BlockSpecialDrop(Material material, MapColor color)
 	{
 		super(material, color);
-	}
-
-	@Override
-	public void harvestBlock(World world, EntityPlayer player, BlockPos pos, IBlockState state, @Nullable TileEntity tileEntity, ItemStack stack)
-	{
-		if (!(tileEntity instanceof IItemWritableTile))
-		{
-			super.harvestBlock(world, player, pos, state, null, stack);
-			return;
-		}
-
-		player.addStat(StatList.getBlockStats(this));
-		player.addExhaustion(0.005F);
-
-		if (world.isRemote)
-		{
-			return;
-		}
-
-		Item item = getItemDropped(state, world.rand, EnchantmentHelper.getEnchantmentLevel(Enchantments.FORTUNE, stack));
-
-		if (item == Items.AIR)
-		{
-			return;
-		}
-
-		ItemStack itemstack = new ItemStack(item, quantityDropped(world.rand));
-		((IItemWritableTile) tileEntity).writeToItem(itemstack);
-		spawnAsEntity(world, pos, itemstack);
 	}
 
 	@Override
@@ -85,5 +48,26 @@ public class BlockSpecialDrop extends Block
 				((IItemWritableTile) tile).readFromItem(stack);
 			}
 		}
+	}
+
+	@Override
+	public void dropBlockAsItemWithChance(World world, BlockPos pos, IBlockState state, float chance, int fortune)
+	{
+	}
+
+	@Override
+	@SuppressWarnings("deprecation")
+	public void breakBlock(World world, BlockPos pos, IBlockState state)
+	{
+		ItemStack stack = super.getItem(world, pos, state);
+		TileEntity tileEntity = world.getTileEntity(pos);
+
+		if (tileEntity instanceof IItemWritableTile)
+		{
+			((IItemWritableTile) tileEntity).writeToItem(stack);
+		}
+
+		spawnAsEntity(world, pos, stack);
+		super.breakBlock(world, pos, state);
 	}
 }
