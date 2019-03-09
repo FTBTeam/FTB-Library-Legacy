a6f2027f1a330b21e739a8c7361d24db1dcf5fb6
LatvianModder <latvianmodder@gmail.com>
2019-03-09 20:35:22 +0200

    Added copy arg to /print_item to copy NBT to your clipboard

--- src/main/java/com/feed_the_beast/ftblib/command/client/CmdPrintItem.java
+++ src/main/java/com/feed_the_beast/ftblib/command/client/CmdPrintItem.java
@@ -2,6 +2,7 @@ package com.feed_the_beast.ftblib.command.client;
 
 import com.feed_the_beast.ftblib.lib.command.CmdBase;
 import com.feed_the_beast.ftblib.lib.util.NBTUtils;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
@@ -11,6 +12,9 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.event.ClickEvent;
+
+import java.util.Arrays;
+import java.util.HashSet;
 
 public class CmdPrintItem extends CmdBase
 {
@@ -34,8 +38,15 @@ public class CmdPrintItem extends CmdBase
 			return;
 		}
 
+		HashSet<String> argsSet = new HashSet<>(Arrays.asList(args));
+
 		ITextComponent component = new TextComponentString(stack.getDisplayName() + " :: " + NBTUtils.getColoredNBTString(stack.serializeNBT()));
 		component.getStyle().setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, stack.serializeNBT().toString()));
 		sender.sendMessage(component);
+
+		if (argsSet.contains("copy"))
+		{
+			GuiScreen.setClipboardString(stack.serializeNBT().toString());
+		}
 	}
 }
